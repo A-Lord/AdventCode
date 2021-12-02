518d08703df17044e34388ae0341d2e46bc0f85d
Adam Lord Erikson <a.LordEriksson@gmail.com>
2021-12-02 16:30:24 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+
+            Console.WriteLine("Hello, World!");
+             var lines = File.ReadAllLines("input.txt");
+             int[] ints = Array.ConvertAll(lines, int.Parse);
+            int timesItsBigger = 0;
+            int lastMeasurment = 0;
+             foreach (var item in ints)
+             {
+                if(lastMeasurment != 0)
+                {
+                    if(item > lastMeasurment)
+                    {
+                         timesItsBigger++;
+                    }                 
+                }
+                lastMeasurment = item;
+             } 
+             Console.WriteLine(timesItsBigger);
